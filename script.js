615f597f0ad686e2ede28f39fe89a97dc1a0d921
ianwambaire <ian.wambaire@strathmore.edu>
2025-01-23 23:40:38 +0300

    updated part 1 variables and datatypes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let name="John Doe";
-let age=25;
-let is_student=true;
+//1Variables and datatypes
+let name="John Doe";//String
+let age=25;//number
+let is_student=true;//boolean
+const cars=["BMW","TOYOTA","HARRIER","MERCEDES"];//array but using type of shows it as an objext
 console.log("Name: "+name+" "+"("+"Type:"+typeof(name)+")");
 console.log("Age: "+age+" "+"("+"Type:"+typeof(age)+")");
 console.log("Is student: "+is_student+" "+"("+"Type:"+typeof(is_student)+")");
+console.log("Car is: "+cars[1]+" "+"("+"Type:"+typeof(cars)+")");
