404f544d5b937b3546d60e1218615c8755f35e12
ianwambaire <ian.wambaire@strathmore.edu>
2025-01-23 23:31:34 +0300

    finished part 1

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let name="John Doe";
-console.log(name+" "+"("+"Type:"+typeof(name)+")");
+let age=25;
+let is_student=true;
+console.log("Name: "+name+" "+"("+"Type:"+typeof(name)+")");
+console.log("Age: "+age+" "+"("+"Type:"+typeof(age)+")");
+console.log("Is student: "+is_student+" "+"("+"Type:"+typeof(is_student)+")");
