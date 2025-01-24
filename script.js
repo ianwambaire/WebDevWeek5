3b032d469e96852cba81410b2d9100c141d0c912
ianwambaire <ian.wambaire@strathmore.edu>
2025-01-24 13:03:12 +0300

    added validation for the input on the calculations

--- script.js
+++ script.js
@@ -21,13 +21,17 @@ console.log("Person is: "+person.fullname()+" "+"("+"Type: "+typeof(person)+")")
 //2.Operators:
 function performingcalculations(){
     let result;
+    //I converted the string inserted by the user to a float
+    //  to be able to perform arithmetic operations on them
     const firstnumber=parseFloat(prompt("Enter the first number: ",""));
     const secondnumber=parseFloat(prompt("Enter the second number: ",""));
+    //I validated the user input to check if the input in a number or not
     if(isNaN(firstnumber)||isNaN(secondnumber)){
         document.getElementById("p").innerHTML="Please enter a valid number";
         return;
     }
     const operation=prompt("Choose an operation: ","(+,-,*,/)");
+    //used a switch case to enable the user choose an operation
     switch(operation){
         case "+":
             result=firstnumber+secondnumber;
@@ -41,10 +45,12 @@ function performingcalculations(){
          case "/":
                 result=firstnumber/secondnumber;
                 break;
-        detault;
+        default:
+            //is to notify the user incase he inputs an invalid operation
           result="Please enter a valid operation(+,-,*,/)";
           return;
 
     }
+    //displays the result on the webpage
     document.getElementById("p").innerHTML=result;
 }
